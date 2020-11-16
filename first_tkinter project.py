3893aef8f21b3c1ffa2a9f4b6f669dcca4cff4a9
Kerr_Braza <33704003+Hanako-Ikezawa@users.noreply.github.com>
2020-11-15 22:10:09 -0600

    updated for basic dict i/o study GUI
    Not finished, gear towards small-scale Japanese study tool?

--- first_tkinter project.py
+++ first_tkinter project.py
@@ -2,10 +2,33 @@
 
 from tkinter import *
 
+#Key down function (events)
+def click():
+    entered_text=textentry.get() #call get on textbox var before
+
 #Main
 window = Tk()
 window.title("Test tkinter project")
+window.configure(background = "lightblue")
 
 #Photo
 photo_1 = PhotoImage(file="Watashi-tachi Nagano Minami Alps.png")
-Label (window, image=photo_1, bg="black") .grid(row=0, column=0, sticky=E)
+photo_1 = photo_1.subsample(4,4) #Basic image mnpl, PIL/Image imports not found
+Label (window, image=photo_1, bg="black") .grid(row=5, column=0, sticky='EWNS')
+#(Sticky East or West > right or left)
+
+#Create Label
+Label (window, text = "Enter a word you want to study/review: ", bg="black", fg="white", font="none 12 bold") .grid(row=0, column=0, sticky='EWNS')
+
+#Text Box
+textentry = Entry(window, width=25, bg="white")
+textentry.grid(row=1, column=0, sticky='EWNS')
+
+#Submit (Buttons)
+Button(window, text="SUBMIT", width=6, command=click) .grid(row=2,column=0, sticky='EWNS')
+
+#Creating a text box
+Label (window, text = "...and its definition is: ", bg="black", fg="white", font="none 12 bold") .grid(row=3, column=0, sticky='EWNS')
+
+output = Text(window, width=75, height=6, wrap=WORD, background="white")
+output.grid(row=4, column=0, sticky='EWNS')
